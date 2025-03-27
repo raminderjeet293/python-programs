1b16a2f158d624f575cba5796d7c56981e6e80b9
raminderjeet293 <raminderjeetkaur02@gmail.com>
2025-03-27 09:50:27 +0530

    calculator

--- assignment2/calculator.py
+++ assignment2/calculator.py
@@ -1,14 +1,3 @@
-# class student:
-#     var1=0
-#     def __init__(self,var):
-#         self.var=var
-#         student.var1+=1
-#     def display(self):
-#         print("Var=",self.var,student.var1)
-# st=student(10)
-# st.display()
-# st1=student(20)
-# st1.display() 
 class cal:
     def __init__(self,n1,n2):
         self.n1=n1
