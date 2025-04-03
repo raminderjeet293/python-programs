2464a49b3a46421190104b9c006c649dbf46e905
raminderjeet293 <raminderjeetkaur02@gmail.com>
2025-04-03 08:49:57 +0530

    Get_string Print_String

---
+++ assignment2/printString.py
@@ -0,0 +1,9 @@
+class MyString: 
+    def get_String(self):
+        self.s = input("enter str:")
+    def print_String(self):
+        print("String entered:", self.s)
+
+obj = MyString() 
+obj.get_String()
+obj.print_String()  
